abbc870139563d3d89a9d4adb87b53c3dd6bc0d7
ctufts <christopher.tufts25@gmail.com>
2015-02-12 11:59:09 -0500

    Reverted to commit 8910ee4 after merging incorrectly between machines (0ed041f)

--- server.R
+++ server.R
@@ -1,10 +1,9 @@
-
 # This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
 #
- 
+
 
 
 shinyServer(function(input, output) {
@@ -12,79 +11,61 @@ shinyServer(function(input, output) {
   set.seed(1)
   
   observe({
-  knn.pred <- knn(data.frame(train.X[,input$checkGroup]),
-                  data.frame(test.X[,input$checkGroup]),
-                  train.Y, k = input$k)
- 
-  
-#   output$value <- renderText({ paste("Classification Error = ",ce(test.Y,knn.pred)) })
-  
-  
-cat( length(knn.pred), length(test.Y)) 
-  output$confusionMatrix <- renderDataTable({
-    # modify this to show title - confusion matrix
-    # /false positive/positive false negative/negative
-    true.positive    <- sum(knn.pred == "positive" & test.Y == "positive")
-    false.positive   <- sum(knn.pred == "negative" & test.Y == "positive")
-    true.negative    <- sum(knn.pred == "negative" & test.Y == "negative")
-    false.negative   <- sum(knn.pred == "positive" & test.Y == "negative")
-    output$value <- renderText({sum(true.positive, false.positive,
-                                    false.negative, true.negative)})
-    row.names <- c("Prediction - FALSE", "Prediction - TRUE" )
-    col.names <- c("Reference - FALSE", "Reference - TRUE")
-    cbind(Outcome = row.names, as.data.frame(matrix( 
-      c(true.negative, false.negative, false.positive, true.positive) ,
-            nrow = 2, ncol = 2, dimnames = list(row.names, col.names))))
+    knn.pred <- knn(data.frame(train.X[,input$checkGroup]),
+                    data.frame(test.X[,input$checkGroup]),
+                    train.Y, k = input$k)
+    
+    
+    #   output$value <- renderText({ paste("Classification Error = ",ce(test.Y,knn.pred)) })
+    
+    
+    cat( length(knn.pred), length(test.Y)) 
+    output$confusionMatrix <- renderDataTable({
+      # modify this to show title - confusion matrix
+      # /false positive/positive false negative/negative
+      true.positive    <- sum(knn.pred == "positive" & test.Y == "positive")
+      false.positive   <- sum(knn.pred == "negative" & test.Y == "positive")
+      true.negative    <- sum(knn.pred == "negative" & test.Y == "negative")
+      false.negative   <- sum(knn.pred == "positive" & test.Y == "negative")
+      output$value <- renderText({sum(true.positive, false.positive,
+                                      false.negative, true.negative)})
+      row.names <- c("Prediction - FALSE", "Prediction - TRUE" )
+      col.names <- c("Reference - FALSE", "Reference - TRUE")
+      cbind(Outcome = row.names, as.data.frame(matrix( 
+        c(true.negative, false.negative, false.positive, true.positive) ,
+        nrow = 2, ncol = 2, dimnames = list(row.names, col.names))))
     }, options = table.settings
-  )
+    )
     
   })
-   
-    observe({
-     
-       output$distPlot <- renderPlot({
-
-         (ggpairs(original.ds[,c(input$pairs.feature.A,
-                                 input$pairs.feature.B,
-                                 input$pairs.feature.C,"num")],
-                  upper = "blank", columns = 1:3,
-                  lower = list(continuous = "points", combo = "dot"),
-                ,color = "num"))
-
-
-         
-       })
-<<<<<<< HEAD
-   })
+  
   observe({
-    input_feature_x <- prop("x",as.symbol(input$featureDisplay_x))
-    input_feature_y <- prop("y",as.symbol(input$featureDisplay_y))
     
-         
-         data.frame(ds) %>%
-           ggvis(x = input_feature_x, y = input_feature_y, 
-                 fill = ~factor(num))%>%
-           layer_points()%>%
-      add_legend("fill",title = "Heart Disease", values = c("negative",  "positive")) %>%
-=======
+    output$distPlot <- renderPlot({
+      
+      (ggpairs(original.ds[,c(input$pairs.feature.A,
+                              input$pairs.feature.B,
+                              input$pairs.feature.C,"num")],
+               upper = "blank", columns = 1:3,
+               lower = list(continuous = "points", combo = "dot"),
+               ,color = "num"))
+      
+      
+      
     })
+  })
   observe({output$feature <- renderText({ (input$featureDisplay) })
            input_feature_x <- prop("x",as.symbol(input$featureDisplay_x))
            input_feature_y <- prop("y",as.symbol(input$featureDisplay_y))
            
-         
-         data.frame(ds) %>%
-           ggvis(x = input_feature_x,input_feature_y, fill = ~factor(num ))%>%
-           layer_points()%>%
-           add_legend("fill", title = "Heart Disease" )%>%
->>>>>>> origin/master
-         bind_shiny("ggvis", "ggvis_ui")
-#       data.frame(ds) %>%
-#         ggvis(x = input_feature_x, fill = ~factor(num))%>%
-#         group_by(num)%>%
-#           layer_histograms(stack = T)%>%
-#         bind_shiny("ggvis", "ggvis_ui")
-        
+           
+           data.frame(ds) %>%
+             ggvis(x = input_feature_x,input_feature_y, fill = ~factor(num ))%>%
+             layer_points()%>%
+             add_legend("fill", title = "Heart Disease" )%>%
+             bind_shiny("ggvis", "ggvis_ui")
+           
+           
   })
-   
+  
 })
--- ui.R
+++ ui.R
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 # This is the user-interface definition of a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
@@ -26,126 +24,37 @@ library(shiny)
 #                    )
 shinyUI(navbarPage("KNN - UCI Heart Disease Data",
                    tabPanel("Results",
-
-                              # Application title
-                    titlePanel("Classification of Heart Disease w/KNN"),
-                  
-                    # Sidebar with a slider input for number of bins
-                    sidebarLayout(
-                      sidebarPanel(
-                        sliderInput("k",
-                                    "number of neighbors",
-                                    min = 1,
-                                    max = 20,
-                                    value = 5),
-                        checkboxGroupInput("checkGroup", label = h3("Dataset Features"), 
-                                           choices = feature.list, inline = T,
-                                           selected = names(feature.list))
-                        
-                      ),
-                  
-                      # Show a plot of the generated distribution
-                      mainPanel(
-                        dataTableOutput('confusionMatrix'),
-                        p('K-Nearest Neighbors...'),
-                        verbatimTextOutput("value")
-                        
-                      )
-                    )
-                    
-                    ), 
-                   tabPanel("EDA",
+                            
+                            # Application title
+                            titlePanel("Classification of Heart Disease w/KNN"),
+                            
+                            # Sidebar with a slider input for number of bins
                             sidebarLayout(
                               sidebarPanel(
-                                selectInput('plot.type', 'Plot Type',
-                                            c("box", "density"),
-                                            selected = "density")
+                                sliderInput("k",
+                                            "number of neighbors",
+                                            min = 1,
+                                            max = 20,
+                                            value = 5),
+                                checkboxGroupInput("checkGroup", label = h3("Dataset Features"), 
+                                                   choices = feature.list, inline = T,
+                                                   selected = names(feature.list))
                                 
                               ),
                               
                               # Show a plot of the generated distribution
                               mainPanel(
-                                plotOutput("distPlot")
+                                dataTableOutput('confusionMatrix'),
+                                p('K-Nearest Neighbors...'),
+                                verbatimTextOutput("value")
+                                
                               )
-                            )),
-                   
-                   
-                   tabPanel("GGVIS",
-                            sidebarLayout(
-                              sidebarPanel(
-                                selectInput("featureDisplay_x", label = h3("X axis"), 
-                                                   choices = feature.list),
-                                selectInput("featureDisplay_y", label = h3("Y axis"), 
-                                            choices = feature.list)
-                              ),
-                              
-                              # Show a plot of the generated distribution
-                              mainPanel(
-                                uiOutput("ggvis_ui"),
-                                ggvisOutput("ggvis"),
-                                verbatimTextOutput("feature")
-                              )
-                            ))
-))
-=======
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
-library(shiny)
-
-# shinyUI(navbarPage("Navbar!",
-#                    tabPanel("Plot",
-#                             sidebarLayout(
-#                               sidebarPanel(
-#                                 radioButtons("plotType", "Plot type",
-#                                              c("Scatter"="p", "Line"="l")
-#                                 )
-#                               ),
-#                               mainPanel(
-#                                 plotOutput("plot")
-#                               )
-#                             )
-#                    ),
-#                    tabPanel("Summary",
-#                             verbatimTextOutput("summary")
-#                    )
-shinyUI(navbarPage("KNN - UCI Heart Disease Data",
-                   tabPanel("Results",
-
-                              # Application title
-                    titlePanel("Classification of Heart Disease w/KNN"),
-                  
-                    # Sidebar with a slider input for number of bins
-                    sidebarLayout(
-                      sidebarPanel(
-                        sliderInput("k",
-                                    "number of neighbors",
-                                    min = 1,
-                                    max = 20,
-                                    value = 5),
-                        checkboxGroupInput("checkGroup", label = h3("Dataset Features"), 
-                                           choices = feature.list, inline = T,
-                                           selected = names(feature.list))
-                        
-                      ),
-                  
-                      # Show a plot of the generated distribution
-                      mainPanel(
-                        dataTableOutput('confusionMatrix'),
-                        p('K-Nearest Neighbors...'),
-                        verbatimTextOutput("value")
-                        
-                      )
-                    )
-                    
-                    ), 
+                            )
+                            
+                   ), 
                    tabPanel("EDA",
-
-                                plotOutput("distPlot"),
+                            
+                            plotOutput("distPlot"),
                             hr(),
                             
                             fluidRow(
@@ -159,7 +68,7 @@ shinyUI(navbarPage("KNN - UCI Heart Disease Data",
                               
                             )
                             
-                  ),
+                   ),
                    
                    
                    tabPanel("GGVIS",
@@ -180,4 +89,3 @@ shinyUI(navbarPage("KNN - UCI Heart Disease Data",
                               )
                             ))
 ))
->>>>>>> origin/master
